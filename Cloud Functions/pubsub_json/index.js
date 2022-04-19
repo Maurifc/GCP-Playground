5e6ae5919a89a98570f34d99a005e7df829a3ec0
maurifcarmo <mauri_vrb@hotmail.com>
2022-04-19 14:21:34 -0300

    Add comments to pubsub_json

--- Cloud Functions/pubsub_json/index.js
+++ Cloud Functions/pubsub_json/index.js
@@ -7,9 +7,18 @@
  * @param {!Object} context Metadata for the event.
  */
 exports.pubsubJson = (event, context) => {
+  // Decode body
   const raw = Buffer.from(event.data, 'base64')
+  
+  // Turn JSON into object
   const obj = JSON.parse(raw)
+
+  // Getting string
   const message = JSON.stringify(raw.toString())
+
+  //Print payload as string
   console.log(message);
+
+  // Print service name (sent on payload)
   console.log("service >> " + obj.service)
 };
